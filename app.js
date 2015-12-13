46507fb766568bc5a6f3f99d7e6b73f96d0d15ea
Andrea Ferlini <AndreaFerlini@MacBook-Pro-di-Ferla.local>
2015-12-13 19:00:21 +0100

    MCV version

--- app.js
+++ app.js
@@ -1,68 +1,88 @@
 /* your code should go in this file */
 
-/* 
+/*
  * The data is available in the array *data*
  * Each element of the array has the following structure:
  *  {
  *    phrase_en : "I'm a man",        // sentence in english
  *    phrase_de : "Ich bin ein Mann"  // sentence in german
  *  }
- */ 
-
-/*
-              <li id="1" class="current">
-                <h3>I'm a man</h3>
-              </li> 
-              <li id="2">
-                <h3>I'm a woman</h3>     
-              </li>    
-              <li id="3">
-                <h3>Hello</h3>     
-              </li>
-*/
-
+ */
 var tmpl = ' <li id="ID">' +
            '  <h3>SENTENCE</h3>' +
            ' </li> ';
 
-var temp1=[];
-
-var fails = 0;
-
 $(document).ready(function(){
+    $(".practice").show();
+    $(".sentences").empty();
     $(".final").hide();
-    $(".sentences").hide();
-    for (var i =0; i < data.length; i++){
-        temp1[i] = tmpl.replace("ID", i+1).replace("SENTENCE", data[i].phrase_en);
-        $(".sentences").append(temp1[i]);
-    }
-    do{
-        var i=0;
-        $(".practice").append(temp1[i]);
-        $("btn opt-continue").click(function(){
-            if(!checkWord($("form-control").val(), i)){
-                fails++;
-            }
-            $(".practice").remove(temp1[i]);
-            i++;
-        });
-        
-    }while(i<data.length);
-    
-    $("#tot-good").append(data.leg-fails);
-    $("#tot").append(data.length);
-    $(".final").hide();
-    
+    controller.init();
 });
 
-function checkWord (word, pos){
-    if (word == data[pos].phrase_en){
-        return true;
-    }else{
-        return false;
-    }
+var model = {
+  collection : data,
+  fails : 0
 };
 
+var controller = {
+  init : function (){
+    view.load();
+  },
+  GetFails : function(){
+    return model.fails;
+  },
+  AddFails : function(){
+    model.fails++;
+    return model.fails;
+  },
+  GetDataLen : function(){
+    return model.collection.length;
+  },
+  GetDataPhrase_en : function(index){
+    return model.collection[index].phrase_en;
+  },
+  GetDataPhrase_de : function(index){
+    return model.collection[index].phrase_de;
+  }
+};
 
+var view = {
+  current : 0,
 
+  load : function (){
+    var exercise;
+    for (var i =0; i < controller.GetDataLen(); i++){
+        exercise = tmpl.replace("ID", i)
+                       .replace("SENTENCE", controller.GetDataPhrase_en(i));
+        $(".sentences").append(exercise);
+    }
+    $(".sentences li").removeClass("current");
+    view.display_phrase();
+    $(".opt-continue").click(function(){
+      //controllo sull'inserimento dell'utente
+      if ($(".form-control").val() != controller.GetDataPhrase_de(view.current)){
+        controller.AddFails();
+      }
+      view.next();
+    })
+  },
 
+  display_phrase : function(){
+    if (this.current == controller.GetDataLen()){
+      $("#tot-good").html(controller.GetDataLen() - controller.GetFails());
+      $("#tot").html(controller.GetDataLen());
+      $(".practice").hide();
+      $(".final").show();
+
+    }else{
+      $("#"+this.current).addClass("current");
+    }
+  },
+
+  next : function (){
+    $(".form-control").val("");
+    this.current ++;
+    $(".sentences li").removeClass("current");
+    view.display_phrase();
+  }
+};
